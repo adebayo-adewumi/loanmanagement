fd98be4f5bf62acc893bcea85b9995bf98900e75
adewumia <adewumia@accessbankplc.com>
2025-01-06 09:59:54 +0100

    add customer repository test

--- src/test/java/com/example/loanmanagement/customer/CustomerRepositoryTest.java
+++ src/test/java/com/example/loanmanagement/customer/CustomerRepositoryTest.java
@@ -1,4 +1,48 @@
 package com.example.loanmanagement.customer;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class CustomerRepositoryTest {
+
+    @Autowired
+    private ICustomerRepository repository;
+
+    @Test
+    public void findByCustomerId() {
+        Customer customer = Customer.builder()
+                .firstname("Adebola")
+                .lastname("Segun")
+                .phone("Segun")
+                .build();
+
+        repository.save(customer);
+
+        var response = repository.findByCustomerId(customer.getId());
+
+        Assertions.assertThat(response).isNotNull();
+    }
+
+    @Test
+    public void findByCustomerPhoneAndEmail() {
+        Customer customer = Customer.builder()
+                .firstname("bello")
+                .lastname("yaya")
+                .phone("08160302602")
+                .email("a@a.com")
+                .build();
+
+        repository.save(customer);
+
+        var response = repository.findByCustomerPhoneAndEmail(customer.getPhone(), customer.getEmail());
+
+        Assertions.assertThat(response).isNotNull();
+    }
+
 }
